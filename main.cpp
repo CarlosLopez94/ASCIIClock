857a434ffbebfb1b7c1f3283708248d418a0c455
Karl <carloslopezrodriguez94@gmail.com>
2019-05-31 00:33:16 +0200

    Added structure

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+
+
+void loadDictionaryASCII();
+
+void printTimeASCII();
+
+int main(){
+    //Init Load ASCII Dictionary
+    loadDictionaryASCII();
+
+    int cont=10;
+    while(cont!=0){
+        //Get current hour
+
+        //Print current hour
+        printTimeASCII();
+
+        //clean screen
+
+
+        cont--;
+    }
+
+    printf("finish");
+    return 0;
+}
+
+void loadDictionaryASCII(){
+
+}
+
+void printTimeASCII(){
+
+}
