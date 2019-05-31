0d6429332fe24537b4f5640b645666a69a4fe11f
Karl <carloslopezrodriguez94@gmail.com>
2019-05-31 22:46:31 +0200

    Added Read Directory files

--- main.cpp
+++ main.cpp
@@ -1,77 +1,95 @@
+#include <dirent.h>
+#include <fstream>
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
-#include <map>
-#include <fstream>
 
-//Declare methods
-std::map<std::string, std::vector<std::string>> loadDictionaryASCII(std::string dictionaryPath);
+// Declare methods
+std::map<std::string, std::vector<std::string>> loadDictionaryASCII(
+    std::string dictionaryPath);
 std::vector<std::string> readFile(std::string filePath);
-void printTimeASCII(std::map<std::string, std::vector<std::string>>& dictionary, std::string currentTime);
+void printTimeASCII(std::map<std::string, std::vector<std::string>> &dictionary,
+                    std::string currentTime);
 
-int main(){
-    //Init Load ASCII Dictionary
-    std::map<std::string, std::vector<std::string>> dictionaryASCII = loadDictionaryASCII("./Dictionary");
+int main() {
+  // Init Load ASCII Dictionary
+  std::map<std::string, std::vector<std::string>> dictionaryASCII =
+      loadDictionaryASCII("./DictionaryASCII/");
 
-    int cont=10;
-    std::string currentTime = "";
-    while(cont!=0){
-        //Get current hour
-        currentTime = "000";
-        //Print current hour
-        printTimeASCII(dictionaryASCII, currentTime);
+  bool end = false;
+  std::string currentTime = "";
+  while (!end) {
+    // Get current hour
+    currentTime = "000";
+    // Print current hour
+    printTimeASCII(dictionaryASCII, currentTime);
 
-        //clean screen
+    // clean screen
 
+    end = true;
+  }
 
-        cont--;
+  return 0;
+}
+
+std::map<std::string, std::vector<std::string>> loadDictionaryASCII(
+    std::string dictionaryDirectoryPath) {
+  // inits map
+  std::map<std::string, std::vector<std::string>> dictionary;
+
+  // Read Dictionary folder content
+  DIR *dp;
+  struct dirent *ep;
+  dp = opendir("./DictionaryASCII/");
+
+  if (dp != NULL) {
+    while (ep = readdir(dp)) {
+      std::string fileName = ep->d_name;
+      std::string filePath = dictionaryDirectoryPath + "/" + fileName;
+      std::vector<std::string> currentFileContent = readFile(filePath);
+      dictionary.insert(std::pair<std::string, std::vector<std::string>>(
+          fileName, currentFileContent));
     }
+    (void)closedir(dp);
 
-    printf("finish");
-    return 0;
+  } else {
+    perror("Couldn't open the directory");
+  }
+
+  return dictionary;
 }
 
-std::map<std::string, std::vector<std::string>> loadDictionaryASCII(std::string dictionaryDirectoryPath){
-    //inits map
-    std::map<std::string, std::vector<std::string>> dictionary;
-   
-   //TODO - read all files from dictionary directory
-    std::vector<std::string> currentFileContent = readFile("./DictionaryASCII/Number0.txt");
+std::vector<std::string> readFile(std::string filePath) {
+  std::vector<std::string> fileContent;
+  std::ifstream fileStream;
 
-    dictionary.insert(std::pair<std::string, std::vector<std::string>>("e", currentFileContent));
-    return dictionary;
-}
-
-
-std::vector<std::string> readFile(std::string filePath){
-    std::vector<std::string> fileContent;
-    std::ifstream fileStream;
-
-    fileStream.open(filePath);
-    printf("Loading '%s'\n", filePath.c_str());
-    std::string currentLine;
-    if(fileStream.is_open()){
-        while(std::getline(fileStream, currentLine)){
-            fileContent.push_back(currentLine);
-        }
-        fileStream.close();
-        printf("File '%s' loaded successfully\n", filePath.c_str());
-    } else {
-			printf("Unable to open '%s' file\n", filePath.c_str());
-	}
-    return fileContent;
-}
-
-void printTimeASCII(std::map<std::string, std::vector<std::string>>& dictionary, std::string currentTime){
-    //Get length of each letter
-    int letterLength = dictionary.begin()->second.size();
-
-    std::string line;
-    for (int i = 0; i < letterLength; i++){
-        line = "";
-        for(char& c : currentTime) {
-            line+= dictionary.find("e")->second.at(i);
-        }
-        printf("%s\n", line.c_str());
+  fileStream.open(filePath);
+  printf("Loading '%s'\n", filePath.c_str());
+  std::string currentLine;
+  if (fileStream.is_open()) {
+    while (std::getline(fileStream, currentLine)) {
+      fileContent.push_back(currentLine);
     }
+    fileStream.close();
+    printf("File '%s' loaded successfully\n", filePath.c_str());
+  } else {
+    printf("Unable to open '%s' file\n", filePath.c_str());
+  }
+  return fileContent;
+}
+
+void printTimeASCII(std::map<std::string, std::vector<std::string>> &dictionary,
+                    std::string currentTime) {
+  // Get length of each letter
+  int letterLength = dictionary.begin()->second.size();
+
+  std::string line;
+  for (int i = 0; i < letterLength; i++) {
+    line = "";
+    for (char &c : currentTime) {
+      line += dictionary.find("e")->second.at(i);
+    }
+    printf("%s\n", line.c_str());
+  }
 }
