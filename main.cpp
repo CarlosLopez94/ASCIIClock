f5d5370d7a6a14dbc427b3e64677ba99ab09f564
Karl <carloslopezrodriguez94@gmail.com>
2019-05-31 21:48:32 +0200

    Added print letters

--- main.cpp
+++ main.cpp
@@ -7,19 +7,19 @@
 //Declare methods
 std::map<std::string, std::vector<std::string>> loadDictionaryASCII(std::string dictionaryPath);
 std::vector<std::string> readFile(std::string filePath);
-void printTimeASCII();
+void printTimeASCII(std::map<std::string, std::vector<std::string>>& dictionary, std::string currentTime);
 
 int main(){
     //Init Load ASCII Dictionary
     std::map<std::string, std::vector<std::string>> dictionaryASCII = loadDictionaryASCII("./Dictionary");
 
-    readFile("./DictionaryASCII/Number0.txt");
     int cont=10;
+    std::string currentTime = "";
     while(cont!=0){
         //Get current hour
-
+        currentTime = "000";
         //Print current hour
-        printTimeASCII();
+        printTimeASCII(dictionaryASCII, currentTime);
 
         //clean screen
 
@@ -35,9 +35,10 @@ std::map<std::string, std::vector<std::string>> loadDictionaryASCII(std::string 
     //inits map
     std::map<std::string, std::vector<std::string>> dictionary;
    
-   //read all files from dictionary directory
-  
-    //dictionary.insert()
+   //TODO - read all files from dictionary directory
+    std::vector<std::string> currentFileContent = readFile("./DictionaryASCII/Number0.txt");
+
+    dictionary.insert(std::pair<std::string, std::vector<std::string>>("e", currentFileContent));
     return dictionary;
 }
 
@@ -52,17 +53,25 @@ std::vector<std::string> readFile(std::string filePath){
     if(fileStream.is_open()){
         while(std::getline(fileStream, currentLine)){
             fileContent.push_back(currentLine);
-            std::cout<<currentLine<<std::endl;
         }
         fileStream.close();
         printf("File '%s' loaded successfully\n", filePath.c_str());
     } else {
 			printf("Unable to open '%s' file\n", filePath.c_str());
 	}
-
     return fileContent;
 }
 
-void printTimeASCII(){
+void printTimeASCII(std::map<std::string, std::vector<std::string>>& dictionary, std::string currentTime){
+    //Get length of each letter
+    int letterLength = dictionary.begin()->second.size();
 
+    std::string line;
+    for (int i = 0; i < letterLength; i++){
+        line = "";
+        for(char& c : currentTime) {
+            line+= dictionary.find("e")->second.at(i);
+        }
+        printf("%s\n", line.c_str());
+    }
 }
