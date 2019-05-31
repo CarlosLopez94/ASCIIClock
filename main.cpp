d889659020ad182bcd1fd5b0ac2e912f3563ad82
Karl <carloslopezrodriguez94@gmail.com>
2019-05-31 21:06:36 +0200

    Added readFile

--- main.cpp
+++ main.cpp
@@ -2,15 +2,18 @@
 #include <string>
 #include <list>
 #include <map>
+#include <fstream>
 
 //Declare methods
-std::map<std::string, std::list<std::string>> loadDictionaryASCII();
+std::map<std::string, std::list<std::string>> loadDictionaryASCII(std::string dictionaryPath);
+std::list<std::string> readFile(std::string filePath);
 void printTimeASCII();
 
 int main(){
     //Init Load ASCII Dictionary
-    std::map<std::string, std::list<std::string>> dictionaryASCII = loadDictionaryASCII();
+    std::map<std::string, std::list<std::string>> dictionaryASCII = loadDictionaryASCII("../Number0.txt");
 
+    readFile("./DictionaryASCII/Number0.txt");
     int cont=10;
     while(cont!=0){
         //Get current hour
@@ -28,8 +31,36 @@ int main(){
     return 0;
 }
 
-std::map<std::string, std::list<std::string>> loadDictionaryASCII(){
+std::map<std::string, std::list<std::string>> loadDictionaryASCII(std::string dictionaryDirectoryPath){
+    //inits map
     std::map<std::string, std::list<std::string>> dictionary;
+   
+   //read all files from dictionary directory
+  
+    //dictionary.insert()
+    return dictionary;
+}
+
+
+std::list<std::string> readFile(std::string filePath){
+    std::list<std::string> fileContent;
+    std::ifstream fileStream;
+
+    fileStream.open(filePath);
+    printf("Loading '%s'\n", filePath.c_str());
+    std::string currentLine;
+    if(fileStream.is_open()){
+        while(std::getline(fileStream, currentLine)){
+            fileContent.push_back(currentLine);
+            std::cout<<currentLine<<std::endl;
+        }
+        fileStream.close();
+        printf("File '%s' loaded successfully\n", filePath.c_str());
+    } else {
+			printf("Unable to open '%s' file\n", filePath.c_str());
+	}
+
+    return fileContent;
 }
 
 void printTimeASCII(){
